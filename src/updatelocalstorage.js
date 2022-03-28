90fec5718be46c77464bfc738f9c596d8d17be47
Dangelo Arrivillaga <98621809+Dangelo-JAN@users.noreply.github.com>
2022-03-28 16:36:52 -0400

    Update updatelocalstorage.js

--- src/updatelocalstorage.js
+++ src/updatelocalstorage.js
@@ -1,1 +1,8 @@
+const UpdateLocalStorage = () => {  
+  for (let i = 0; i < newTodoes.length; i += 1) {
+    newTodoes[i].index = i + 1;
+  }
+  localStorage.setItem('todoes', JSON.stringify(newTodoes));
+}
 
+export default UpdateLocalStorage;
